ce3350a8fd16d4e94cb3bac259ed2d3c2bf250c0
Kittisak Kittisak <59143205@g.amru.ac.th>
2019-07-13 14:14:48 +0700

    เพิ่มหน้าเทส

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => res.send('Kittisak Kraiudom!'))
-app.get('/about', (req, res) => res.send('หน้า about'))
-app.get('/contact', (req, res) => res.send('น้องแนท676 tell: 0841891343(ได้ทุกที่ยกเว้นข้างหลัง)' ))
+app.get('/about', (req, res) => res.send('0891891355'))
+app.get('/test', (req, res) => {
+    res.json({message: "test"})
+
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
